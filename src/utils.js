b520bbc22657a553e208385989178b1689a88a09
Rayed Bin Wahed <rayed.bin.wahed@g.bracu.ac.bd>
2020-06-10 22:51:47 +0600

    feat: Implement static version

---
+++ src/utils.js
@@ -0,0 +1,42 @@
+export const PRODUCTS = [
+  {
+    category: "Sporting Goods",
+    price: "$49.99",
+    stocked: true,
+    name: "Football",
+  },
+  {
+    category: "Sporting Goods",
+    price: "$9.99",
+    stocked: true,
+    name: "Baseball",
+  },
+  {
+    category: "Sporting Goods",
+    price: "$29.99",
+    stocked: false,
+    name: "Basketball",
+  },
+  {
+    category: "Electronics",
+    price: "$99.99",
+    stocked: true,
+    name: "iPod Touch",
+  },
+  {
+    category: "Electronics",
+    price: "$399.99",
+    stocked: false,
+    name: "iPhone 5",
+  },
+  { category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7" },
+];
+
+export const sortProductByCategory = (firstProduct, secondProduct) => {
+  const firstCategory = firstProduct.category.toLowerCase();
+  const secondCategory = secondProduct.category.toLowerCase();
+
+  if (firstCategory < secondCategory) return -1;
+  else if (firstCategory > secondCategory) return 1;
+  else return 0;
+};
