f04cc90a2f0b9e4e79718c86fca181f4ffefe20d
Rayed Bin Wahed <rayed.bin.wahed@g.bracu.ac.bd>
2020-06-10 22:51:47 +0600

    feat: Implement static version

--- src/index.js
+++ src/index.js
@@ -1,17 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { PRODUCTS, sortProductByCategory } from "./utils";
+
+const ProductRow = ({ product }) => {
+  const name = product.stocked ? (
+    product.name
+  ) : (
+    <span style={{ color: "red" }}>{product.name}</span>
+  );
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+};
+
+const ProductCategoryRow = ({ category }) => {
+  return (
+    <tr>
+      <th colSpan="2">{category}</th>
+    </tr>
+  );
+};
+
+const ProductTable = ({ products }) => {
+  const productRows = [];
+  let lastCategory = null;
+
+  products.sort(sortProductByCategory).forEach(product => {
+    if (product.category !== lastCategory) {
+      productRows.push(
+        <ProductCategoryRow
+          key={product.category}
+          category={product.category}
+        />
+      );
+    }
+
+    productRows.push(<ProductRow key={product.name} product={product} />);
+
+    lastCategory = product.category;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{productRows}</tbody>
+    </table>
+  );
+};
+
+const SearchBar = () => {
+  return (
+    <form>
+      <input type="text" placeholder="Search..." />
+      <p>
+        <input type="checkbox" /> &nbsp; Only show products in stock
+      </p>
+    </form>
+  );
+};
+
+const FilterableProductTable = ({ products }) => {
+  return (
+    <>
+      <SearchBar />
+      <ProductTable products={products} />
+    </>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FilterableProductTable products={PRODUCTS} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
